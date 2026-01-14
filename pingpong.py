5fcdcf922419b75ed8a66c83653e9cc9f7f95c63
parsinartem09-ai <parsinartem09@gmail.com>
2026-01-14 19:14:18 +0400

    игрок, окно, мяч с отскоком

--- pingpong.py
+++ pingpong.py
@@ -19,7 +19,30 @@ class Player():
         if self.hitbox.top < 0:
             self.hitbox.top = 0
 
+class Ball():
+    def __init__(self, x, y, speed):
+        self.hitbox = Rect(x, y, 30, 30)
+        self.speed = speed
+        self.speedx = speed
+        self.speedy = speed
+    
+    def move(self):
+        self.hitbox.x += self.speedx
+        self.hitbox.y += self.speedy
+        if self.hitbox.top < 0:
+            self.speedy = self.speed
+        if self.hitbox.bottom > 480:
+            self.speedy = -self.speed
+        if self.hitbox.left < 0:
+            self.speedx = self.speed
+        if self.hitbox.right > 720:
+            self.speedx = -self.speed
+        if self.hitbox.colliderect(player1.hitbox):
+            self.speedx = self.speed
+
+
 player1 = Player(50, 240, 5)
+ball = Ball(360, 240, 3)
 
 while True:
     win.fill((255, 255, 255))
@@ -29,5 +52,7 @@ while True:
             exit()
     player1.move()
     draw.rect(win, (255, 0, 0), player1.hitbox)
+    ball.move()
+    draw.rect(win, (0, 255, 0), ball.hitbox)
     display.update()
     clock.tick(200)
