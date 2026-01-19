0db4be1d03db05c715fe61c140bfeb2057c38c54
parsinartem09-ai <parsinartem09@gmail.com>
2026-01-19 18:41:27 +0400

    окно, два игрока, мяч, два вида автопилота, голы

--- pingpong.py
+++ pingpong.py
@@ -1,4 +1,5 @@
 from pygame import *
+from time import sleep
 
 win = display.set_mode((720, 480))
 clock = time.Clock()
@@ -18,6 +19,23 @@ class Player():
             self.hitbox.bottom = 480
         if self.hitbox.top < 0:
             self.hitbox.top = 0
+    
+    def autopilotV1(self):
+        self.hitbox.centery = ball.hitbox.centery
+        if self.hitbox.bottom > 480:
+            self.hitbox.bottom = 480
+        if self.hitbox.top < 0:
+            self.hitbox.top = 0
+
+    def autopilotV2(self):
+        if self.hitbox.bottom > 480:
+            self.hitbox.bottom = 480
+        if self.hitbox.top < 0:
+            self.hitbox.top = 0
+        if ball.hitbox.centery > self.hitbox.centery:
+            self.hitbox.centery += self.speed
+        if ball.hitbox.centery < self.hitbox.centery:
+            self.hitbox.centery -= self.speed
 
 class Ball():
     def __init__(self, x, y, speed):
@@ -35,14 +53,21 @@ class Ball():
             self.speedy = -self.speed
         if self.hitbox.left < 0:
             self.speedx = self.speed
+            self.hitbox.center = (360, 240)
+            sleep(1)
         if self.hitbox.right > 720:
             self.speedx = -self.speed
-        if self.hitbox.colliderect(player1.hitbox):
-            self.speedx = self.speed
+            self.hitbox.center = (360, 240)
+            sleep(1)
+        for player in player_list:
+            if self.hitbox.colliderect(player.hitbox):
+                self.speedx *= -1
 
 
 player1 = Player(50, 240, 5)
 ball = Ball(360, 240, 3)
+player2 = Player(670, 240, 1)
+player_list = [player1, player2]
 
 while True:
     win.fill((255, 255, 255))
@@ -52,6 +77,8 @@ while True:
             exit()
     player1.move()
     draw.rect(win, (255, 0, 0), player1.hitbox)
+    player2.autopilotV2()
+    draw.rect(win, (255, 0, 0), player2.hitbox)
     ball.move()
     draw.rect(win, (0, 255, 0), ball.hitbox)
     display.update()
